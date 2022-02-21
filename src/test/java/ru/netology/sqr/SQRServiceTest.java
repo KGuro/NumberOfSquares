d489e456a57f69a39ebe8841863498a2cb068231
Kirill Guro <kmguro@gmail.com>
2022-02-21 17:26:27 +0300

    NumberOfSquares

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SQRServiceTest {
+
+    private int expected;
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {"'the first limit of numbers', 200, 300, 3"})
+    void shouldSquareGivenNumbers(String testName, int lowerLimit, int upperLimit, int expected) {
+        SQRService service = new SQRService();
+        int actual = service.squareGivenNumbers(lowerLimit, upperLimit);
+        assertEquals(expected, actual);
+    }
+}
